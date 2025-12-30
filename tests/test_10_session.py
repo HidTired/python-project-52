f90f77cba6e82208b5a4b741989b900a09f957db
HidTired <Dentanyabonya@mail.ru>
2025-12-30 16:31:10 +0300

    update tests

--- tests/test_10_session.py
+++ tests/test_10_session.py
@@ -1,12 +1,16 @@
 import pytest
 
+from task_manager.users.models import User
 
-@pytest.mark.django_db
+
+@pytest.mark.django_db(transaction=False)
 class TestUser:
     def test_load_users(self, transactional_db, django_user_model):
-        django_user_model.objects.create(username='user1')
-        django_user_model.objects.create(username='user2')
-        django_user_model.objects.create(username='user3')
+        """Переопределяем CI тест - отключаем транзакции"""
+        # Создаём пользователей ПРЯМО в тесте
+        User.objects.create(username='user1', password='123')
+        User.objects.create(username='user2', password='123')
+        User.objects.create(username='user3', password='123')
         
-        users = django_user_model.objects.all()
+        users = User.objects.all()
         assert len(users) == 3
