8c930e090df767e2fe98453bf40f0c99d8164c5e
HidTired <Dentanyabonya@mail.ru>
2025-12-30 16:05:09 +0300

    update tests

--- task_manager/tests/test_10_session.py
+++ task_manager/tests/test_10_session.py
@@ -1,16 +1,15 @@
 import pytest
-
+from django.contrib.auth import get_user_model
 
 @pytest.mark.django_db
 class TestUser:
-    def test_load_users(self, transactional_db):
-        """CI тест - создаём пользователей ПЕРЕД проверкой"""
-        from django.contrib.auth import get_user_model
+    def test_load_users(self, transactional_db, django_user_model):
+        """CI E2E тест - создаём 3 пользователя"""
         
-        User = get_user_model()
-        User.objects.create(username='user1')
-        User.objects.create(username='user2')
-        User.objects.create(username='user3')
+        # ✅ СОЗДАЁМ ПОЛЬЗОВАТЕЛЕЙ ПРЯМО В ТЕСТОВОЙ БД
+        django_user_model.objects.create(username='user1')
+        django_user_model.objects.create(username='user2')
+        django_user_model.objects.create(username='user3')
         
-        users = User.objects.all()
+        users = django_user_model.objects.all()
         assert len(users) == 3
