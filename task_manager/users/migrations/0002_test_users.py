adb3dc16bbaa6f67e65ca10eeba72f1e681b4359
HidTired <Dentanyabonya@mail.ru>
2025-12-30 16:02:38 +0300

    update tests

--- task_manager/users/migrations/0002_test_users.py
+++ task_manager/users/migrations/0002_test_users.py
@@ -1,23 +1,15 @@
 from django.db import migrations
-from django.contrib.auth.hashers import make_password
 
 def create_test_users(apps, schema_editor):
-    """Создаёт 3 пользователя для CI E2E тестов"""
+    """Создаёт 3 пользователя БЕЗ паролей для CI тестов"""
     User = apps.get_model('users', 'User')
     
-    # Создаём пользователей с паролями
-    users = [
+    # Создаём пользователей без паролей (для E2E теста достаточно username)
+    User.objects.bulk_create([
         User(username='user1', first_name='John', last_name='Doe'),
         User(username='user2', first_name='Jane', last_name='Smith'),
         User(username='user3', first_name='Bob', last_name='Johnson'),
-    ]
-    
-    # Устанавливаем пароли после создания (bulk_create не работает с паролями)
-    for user in users:
-        user.set_password('testpass123')
-        user.save()
-    
-    User.objects.bulk_create(users, ignore_conflicts=True)
+    ], ignore_conflicts=True)
 
 def reverse_create_test_users(apps, schema_editor):
     """Удаляет тестовых пользователей"""
