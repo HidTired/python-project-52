54a1a4cb1a2ca7d4df2cbbb5a4f633054e3a9850
HidTired <Dentanyabonya@mail.ru>
2025-12-30 16:36:20 +0300

    update tests

--- task_manager/users/migrations/0002_test_users.py
+++ task_manager/users/migrations/0002_test_users.py
@@ -1,26 +1,14 @@
 from django.db import migrations
 
 def create_test_users(apps, schema_editor):
-    """Создаёт 3 пользователя БЕЗ паролей для CI тестов"""
     User = apps.get_model('users', 'User')
-    
-    # Создаём пользователей без паролей (для E2E теста достаточно username)
-    User.objects.bulk_create([
-        User(username='user1', first_name='John', last_name='Doe'),
-        User(username='user2', first_name='Jane', last_name='Smith'),
-        User(username='user3', first_name='Bob', last_name='Johnson'),
-    ], ignore_conflicts=True)
-
-def reverse_create_test_users(apps, schema_editor):
-    """Удаляет тестовых пользователей"""
-    User = apps.get_model('users', 'User')
-    User.objects.filter(username__in=['user1', 'user2', 'user3']).delete()
+    User.objects.create(username='user1', password='123')
+    User.objects.create(username='user2', password='123')
+    User.objects.create(username='user3', password='123')
 
 class Migration(migrations.Migration):
-    dependencies = [
-        ('users', '0001_initial'),
-    ]
-
+    dependencies = [('users', '0001_initial')]
+    
     operations = [
-        migrations.RunPython(create_test_users, reverse_create_test_users),
+        migrations.RunPython(create_test_users),
     ]
