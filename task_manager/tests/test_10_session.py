fa180effbbb0cea4d65b11f13dbc052e982cd671
HidTired <Dentanyabonya@mail.ru>
2025-12-30 04:03:23 +0300

    update tests

---
+++ task_manager/tests/test_10_session.py
@@ -0,0 +1,15 @@
+import pytest
+from django.contrib.auth import get_user_model
+from pytest_django import django_user_model
+
+@pytest.mark.django_db
+class TestUser:
+    def test_load_users(self, transactional_db, django_user_model):
+        """CI тест - создаём пользователей ПЕРЕД проверкой"""
+        
+        django_user_model.objects.create(username='user1')
+        django_user_model.objects.create(username='user2')
+        django_user_model.objects.create(username='user3')
+        
+        users = django_user_model.objects.all()
+        assert len(users) == 3
