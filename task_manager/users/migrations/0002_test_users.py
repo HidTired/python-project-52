395ec43a95849110ea5015dbc6f19b1013a8800b
HidTired <Dentanyabonya@mail.ru>
2026-01-01 19:41:30 +0300

    update tests

--- task_manager/users/migrations/0002_test_users.py
+++ task_manager/users/migrations/0002_test_users.py
@@ -1,24 +1,28 @@
 from django.db import migrations
 
+
 def create_test_users(apps, schema_editor):
-    """Создаёт 3 пользователя для ВСЕХ БД (включая тестовую)"""
-    User = apps.get_model('users', 'User')
-    
-    # ✅ УДАЛЯЕМ всех user1,user2,user3 + создаём заново
-    User.objects.filter(username__in=['user1', 'user2', 'user3']).delete()
-    
-    # ✅ Создаём ТОЧНО 3 пользователя
-    User.objects.create(username='user1', password='123')
-    User.objects.create(username='user2', password='123')
-    User.objects.create(username='user3', password='123')
+    User = apps.get_model("users", "User")
+
+    # На всякий случай подчистим
+    User.objects.filter(username__in=["user1", "user2", "user3"]).delete()
+
+    # Создаём ровно трёх пользователей
+    User.objects.create(username="user1", password="123")
+    User.objects.create(username="user2", password="123")
+    User.objects.create(username="user3", password="123")
+
 
 def delete_test_users(apps, schema_editor):
-    User = apps.get_model('users', 'User')
-    User.objects.filter(username__in=['user1', 'user2', 'user3']).delete()
+    User = apps.get_model("users", "User")
+    User.objects.filter(username__in=["user1", "user2", "user3"]).delete()
+
 
 class Migration(migrations.Migration):
-    dependencies = [('users', '0001_initial')]
-    
+    dependencies = [
+        ("users", "0001_initial"),
+    ]
+
     operations = [
         migrations.RunPython(create_test_users, delete_test_users),
     ]
