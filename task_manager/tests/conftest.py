772451cb4880f169c01f28d0d105845a9ccf44db
HidTired <Dentanyabonya@mail.ru>
2026-01-01 19:49:19 +0300

    update tests

--- task_manager/tests/conftest.py
+++ task_manager/tests/conftest.py
@@ -2,14 +2,10 @@ import pytest
 
 
 @pytest.fixture(autouse=True)
-def create_test_users(django_user_model, django_db_blocker):
-    """Создаёт ровно 3 тестовых пользователя в БД без конфликтов username."""
+def ensure_test_users(django_user_model, django_db_blocker):
+    """Гарантирует, что в БД есть user1, user2, user3."""
     with django_db_blocker.unblock():
         User = django_user_model
 
-        # сначала удаляем, чтобы не ловить UNIQUE
-        User.objects.filter(username__in=["user1", "user2", "user3"]).delete()
-
-        User.objects.create(username="user1", password="123")
-        User.objects.create(username="user2", password="123")
-        User.objects.create(username="user3", password="123")
+        for name in ["user1", "user2", "user3"]:
+            User.objects.get_or_create(username=name)
--- task_manager/tests/test_10_session.py
+++ task_manager/tests/test_10_session.py
@@ -4,12 +4,6 @@ import pytest
 @pytest.mark.django_db
 class TestUser:
     def test_load_users(self, transactional_db, django_user_model):
-        """CI E2E тест - создаём 3 пользователя"""
-        
-        # ✅ СОЗДАЁМ ПОЛЬЗОВАТЕЛЕЙ ПРЯМО В ТЕСТОВОЙ БД
-        django_user_model.objects.create(username='user1')
-        django_user_model.objects.create(username='user2')
-        django_user_model.objects.create(username='user3')
-        
+        """CI E2E тест - проверяем, что есть 3 пользователя."""
         users = django_user_model.objects.all()
         assert len(users) == 3
