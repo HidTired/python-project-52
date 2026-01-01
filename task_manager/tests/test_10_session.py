c4455ed19274de9c94fdd201b3a5e4880156e18f
HidTired <Dentanyabonya@mail.ru>
2026-01-01 20:13:53 +0300

    update tests

--- task_manager/tests/test_10_session.py
+++ task_manager/tests/test_10_session.py
@@ -3,6 +3,6 @@ import pytest
 
 @pytest.mark.django_db
 class TestUser:
-    def test_load_users(self, transactional_db, django_user_model):
+    def test_load_users(self, django_user_model):
         users = django_user_model.objects.all()
         assert len(users) == 3
