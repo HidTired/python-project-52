9040a48f671f044e0a89dcc3d045624967c94751
HidTired <Dentanyabonya@mail.ru>
2026-01-01 20:07:05 +0300

    update tests

--- task_manager/tests/test_10_session.py
+++ task_manager/tests/test_10_session.py
@@ -3,11 +3,10 @@ import pytest
 
 @pytest.fixture(autouse=True)
 def ensure_test_users(django_user_model, django_db_blocker):
-    """Гарантирует, что в тестовой БД есть user1, user2, user3."""
     with django_db_blocker.unblock():
         User = django_user_model
-        for name in ["user1", "user2", "user3"]:
-            User.objects.get_or_create(username=name)
+        for username in ["user1", "user2", "user3"]:
+            User.objects.get_or_create(username=username)
 
 
 @pytest.mark.django_db
