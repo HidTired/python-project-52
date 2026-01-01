4f3793199f10162f536b74b929d9abcc28058c42
HidTired <Dentanyabonya@mail.ru>
2026-01-01 20:10:58 +0300

    update tests

--- task_manager/tests/conftest.py
+++ task_manager/tests/conftest.py
@@ -8,10 +8,3 @@ def ensure_test_users(django_user_model, django_db_blocker):
         User = django_user_model
         for name in ["user1", "user2", "user3"]:
             User.objects.get_or_create(username=name)
-
-
-@pytest.mark.django_db
-class TestUser:
-    def test_load_users(self, transactional_db, django_user_model):
-        users = django_user_model.objects.all()
-        assert len(users) == 3
--- task_manager/tests/test_10_session.py
+++ task_manager/tests/test_10_session.py
@@ -1,14 +1,6 @@
 import pytest
 
 
-@pytest.fixture(autouse=True)
-def ensure_test_users(django_user_model, django_db_blocker):
-    with django_db_blocker.unblock():
-        User = django_user_model
-        for username in ["user1", "user2", "user3"]:
-            User.objects.get_or_create(username=username)
-
-
 @pytest.mark.django_db
 class TestUser:
     def test_load_users(self, transactional_db, django_user_model):
