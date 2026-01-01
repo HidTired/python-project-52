0137f8c4dc055ceea46f58074985efda5384c2c4
HidTired <Dentanyabonya@mail.ru>
2026-01-01 20:18:42 +0300

    update tests

--- task_manager/tests/test_10_session.py
+++ task_manager/tests/test_10_session.py
@@ -4,5 +4,11 @@ import pytest
 @pytest.mark.django_db
 class TestUser:
     def test_load_users(self, django_user_model):
-        users = django_user_model.objects.all()
+        User = django_user_model
+
+        # ЯВНО создаю тестовых пользователей в тестовой БД
+        for name in ["user1", "user2", "user3"]:
+            User.objects.get_or_create(username=name)
+
+        users = User.objects.all()
         assert len(users) == 3
