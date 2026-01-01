af65e7e20fd0a9887f2e7dd12706f2bac1495526
HidTired <Dentanyabonya@mail.ru>
2026-01-01 20:29:32 +0300

    update tests

--- task_manager/tests/test_10_session.py
+++ task_manager/tests/test_10_session.py
@@ -6,15 +6,12 @@ class TestUser:
     def test_load_users(self, django_user_model):
         User = django_user_model
 
-        print("=== TEST_10_SESSION STARTED ===")
+        print("== creating users in test ==")
 
-        # Создаём тестовых пользователей
         for name in ["user1", "user2", "user3"]:
-            u, created = User.objects.get_or_create(username=name)
-            print("created:", name, "->", created)
+            User.objects.get_or_create(username=name)
 
-        print("=== QUERY USERS ===")
-        print("count():", User.objects.count())
-        print("all():", list(User.objects.values_list("username", flat=True)))
+        users = User.objects.all()
+        print("== users count ==", users.count())
 
-        assert User.objects.count() == 3
+        assert users.count() == 3
