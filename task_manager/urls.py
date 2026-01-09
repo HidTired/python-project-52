142c8086a8c8f0e3813760dcaaed829c4334cc20
HidTired <Dentanyabonya@mail.ru>
2026-01-10 01:42:58 +0300

    update tests

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -4,9 +4,9 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('statuses/', include('statuses.urls')),
-    path('tasks/', include('tasks.urls')),
-    path('labels/', include('labels.urls')),
+    path('users/', include('task_manager.users.urls')),
+    path('statuses/', include('task_manager.statuses.urls')),
+    path('tasks/', include('task_manager.tasks.urls')),
+    path('labels/', include('task_manager.labels.urls')),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
