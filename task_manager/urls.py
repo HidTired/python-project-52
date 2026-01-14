4e09a0bf6e6959690256db1439f57bed1d131a66
HidTired <Dentanyabonya@mail.ru>
2026-01-15 00:47:51 +0300

    готовый READ.ME

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('statuses/', include('task_manager.statuses.urls')),
     path('tasks/', include('task_manager.tasks.urls')),
     path('labels/', include('task_manager.labels.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
